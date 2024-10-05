40ad0e7455cc807d9c40e46db96147882d18b2e5
Sirius <sirius2051@protonmail.ch>
2024-10-05 00:04:49 -0400

    chore: change styles, add validations and save operation on localStorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,25 @@
 var screen = document.getElementById('screen');
 var clean = document.getElementById('clean');
 var result = document.getElementById('result');
+// counter = localStorage.length;
+var operations = [
+    "+",
+    "-",
+    "*",
+    "/",
+    "**"
+]
 function addKey(value) {
-    console.log(value);
+    if (operations.includes(value) && operations.includes(screen.value[screen.value.length - 1]) ) {
+        return false;
+    }
     screen.value += value;
 }
 clean.addEventListener('click', () => {
     screen.value = "";
 });
 result.addEventListener('click', () => {
+    var data = {operation: screen.value, result: eval(screen.value)};
+    localStorage.setItem(localStorage.length, JSON.stringify(data))
     screen.value = eval(screen.value);
 });
